248ea0c0381e1d61c7f0ecf5a18fa2e6e0220a15
meenakshimadugula95 <meenu2795@gmail.com>
2020-04-23 19:04:05 -0400

    Added preprocessing code

--- lstm.py
+++ lstm.py
@@ -12,10 +12,52 @@ import re
 import pickle
 import csv
 from sklearn.metrics import classification_report, confusion_matrix
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+
 
 X_train_or = pd.read_csv('data/train.csv',names=["headline","y"])
 X_valid_or = pd.read_csv('data/valid.csv',names=["headline","y"])
 X_test_or = pd.read_csv('data/test.csv',names=["headline","y"])
+
+stop_words = set(stopwords.words('english'))
+tokenizer = RegexpTokenizer(r'\w+')
+lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
+
+def preprocess(data):
+
+    new_data=[]
+    for iter in range(len(data)):
+        curText=data.iloc[iter,0]
+        if len(curText) > 0:
+            ascii_data = re.sub('[^A-Za-z0-9\']+', ' ', curText)
+            text = [w for w in ascii_data.split() if w not in stop_words]
+            lem_text = [lemmatizer.lemmatize(i) for i in text]
+            stem_text = ""
+            for abc in lem_text:
+                # stem = "".join([stemmer.stem(i) for i in abc])
+                stem = "".join([i for i in abc])
+                stem_text = stem_text + " " + stem
+            new_data.append(stem_text)
+        else:
+            new_data.append(np.NaN)
+    data['headline']=new_data
+      
+    return data
+
+# print(X_train_or.iloc[1,0])
+X_train_pre = preprocess(X_train_or)
+X_valid_pre = preprocess(X_valid_or)
+X_test_pre = preprocess(X_test_or)
+# X_train_pre = X_train_or
+# X_valid_pre = X_valid_or
+# X_test_pre = X_test_or
+# print(X_train_pre.iloc[1,0])
 
 # The maximum number of words to be used. (most frequent)
 MAX_NB_WORDS = 50000
@@ -25,38 +67,38 @@ MAX_SEQUENCE_LENGTH = 64
 EMBEDDING_DIM = 100
 
 tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
-tokenizer.fit_on_texts(X_train_or['headline'].values)
+tokenizer.fit_on_texts(X_train_pre['headline'].values)
 word_index = tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
 
-X_train = tokenizer.texts_to_sequences(X_train_or['headline'].values) # Every word got a new number
+X_train = tokenizer.texts_to_sequences(X_train_pre['headline'].values) # Every word got a new number
 # print(X_train[1])
 
 X_train = pad_sequences(X_train, maxlen=MAX_SEQUENCE_LENGTH)
 print('Shape of train data tensor:', X_train.shape)
 # print(X_train[1])
 
-X_test = tokenizer.texts_to_sequences(X_test_or['headline'].values) # Every word got a new number
+X_test = tokenizer.texts_to_sequences(X_test_pre['headline'].values) # Every word got a new number
 # print(X_test[1])
 
 X_test = pad_sequences(X_test, maxlen=MAX_SEQUENCE_LENGTH)
 print('Shape of test data tensor:', X_test.shape)
 # print(X_test[1])
 
-X_valid = tokenizer.texts_to_sequences(X_valid_or['headline'].values) # Every word got a new number
+X_valid = tokenizer.texts_to_sequences(X_valid_pre['headline'].values) # Every word got a new number
 # print(X_valid[1])
 
 X_valid = pad_sequences(X_valid, maxlen=MAX_SEQUENCE_LENGTH)
 print('Shape of valid data tensor:', X_valid.shape)
 # print(X_valid[1])
 
-Y_train = pd.get_dummies(X_train_or['y']).values
+Y_train = pd.get_dummies(X_train_pre['y']).values
 print('Shape of train label tensor:', Y_train.shape)
 
-Y_test = pd.get_dummies(X_test_or['y']).values
+Y_test = pd.get_dummies(X_test_pre['y']).values
 print('Shape of test label tensor:', Y_test.shape)
 
-Y_valid = pd.get_dummies(X_valid_or['y']).values
+Y_valid = pd.get_dummies(X_valid_pre['y']).values
 print('Shape of valid label tensor:', Y_valid.shape)
 
 model = Sequential()
